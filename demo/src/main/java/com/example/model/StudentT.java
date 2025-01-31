0337841d6ef62e317f20cc7558617b4f0d1826a6
kool-pixel01 <kuldip24fasate@gmail.com>
2025-01-31 08:10:55 +0530

    Update

--- demo/src/main/java/com/example/model/StudentT.java
+++ demo/src/main/java/com/example/model/StudentT.java
@@ -7,11 +7,9 @@ public class StudentT {
 	}
 	public void m2(){
 	}
-<<<<<<< HEAD
 	public void m4() {
-=======
+	}
 	public void m3(){
->>>>>>> branch 'master' of https://github.com/kool-pixel01/TestJavaCode
 		
 	}
 	
